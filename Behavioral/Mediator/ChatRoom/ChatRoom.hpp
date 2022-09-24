794552eec161fdbdbbcf07a32a04dad8e5666b21
Gabriel Feceoru <gabi.feceoru@gmail.com>
2022-09-24 10:25:54 +0200

    Add Mediator - Chatroom

---
+++ Behavioral/Mediator/ChatRoom/ChatRoom.hpp
@@ -0,0 +1,11 @@
+#pragma once
+#include <algorithm>
+
+struct ChatRoom
+{
+  vector<Person*> people;
+
+  void broadcast(const string& origin, const string& message);
+  void join(Person* p);
+  void message(const string& origin, const string& who, const string& message);
+};
---
+++ Behavioral/Mediator/ChatRoom/Person.hpp
@@ -0,0 +1,22 @@
+#pragma once
+#include <string>
+#include <vector>
+using namespace std;
+
+struct ChatRoom;
+
+struct Person {
+  string name;
+  ChatRoom* room{nullptr};
+
+  Person(const string &name);
+
+  void say(const string& message) const;
+  void pm(const string& who, const string& message) const;
+  void receive(const string& origin, const string& message);
+
+  bool operator==(const Person &rhs) const;
+  bool operator!=(const Person &rhs) const;
+};
+
+
